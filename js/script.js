287e90917ce32f669162b0c6685468ae14109e29
Gabrielle Piccininni <piccingt@gmail.com>
2016-05-03 17:49:15 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -5,28 +5,44 @@
 
 
 $(document).ready(function(){
-	var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+	
     
     $('form').submit(cityBackground);
 
     //when a user clicks on thr dropdown, view all options
-    $("#city-type").click(function(){
+    $('#city-type').click(function(){
+        var cities = ['NYC', 'SF', 'LA', 'ATX', 'SYD'];
         $("#city-type").append(" <option>Los Angeles</option>, <option>New York City</option>,<option>San Francisco</option>, <option>Sydney</option>, ").unbind();
     });
    
     //when user selects a city in the dropdown, get the value of that city
     function cityBackground(event){
 		event.preventDefault();
-		cities = $('#city-type').val(); 
+		var cities = $('#city-type').val(); 
 
-		console.log(city-type)
+		console.log(cities)
+	}
 
-		
-		if (city === "New York City") {
-				$('body').removeClass();
-				$('body').addClass('nyc');
-			}			
+	//match the value of that city with the relevant index
 
+	if (cities === 'NYC') {
+		// Show the NYC background
+		$('body').addClass('nyc');
+	} else if (cities === 'SF') {
+		// Show the SF background
+		$('body').addClass('sf');
+	} else if (cities === 'LA') {
+		// Show the LA background
+		$('body').addClass('la');
+	} else if (cities === 'ATX') {
+		// Show the LA background
+		$('body').addClass('austin');
+	} else if (cities === 'SYD') {
+		// Show the LA background
+		$('body').addClass('sydney');
+	} else {
+		// Remove all classes from body to show the default background.
+		$('body').removeClass();
 	}
 
 	
