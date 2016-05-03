e8b3adc15a3be7dbef1975e268c7cec7d4ab812f
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2016-05-03 19:03:23 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -9,42 +9,53 @@ $(document).ready(function(){
     
     $('form').submit(cityBackground);
 
+    // If you define your array outside of the function, it is not dependent on the function
+    var cities = ['NYC', 'SF', 'LA', 'ATX', 'SYD'];
+
+    // You want the cities to load when the page loads
+    for (var i=0; i<cities.length; i++){
+      $("#city-type").append(" <option>"+ cities[i] + "</option>").unbind();
+
+    }
+
     //when a user clicks on thr dropdown, view all options
-    $('#city-type').click(function(){
-        var cities = ['NYC', 'SF', 'LA', 'ATX', 'SYD'];
-        $("#city-type").append(" <option>Los Angeles</option>, <option>New York City</option>,<option>San Francisco</option>, <option>Sydney</option>, ").unbind();
-    });
+    // Change is a great function to use here because it isn't the form submitting when the value gets changed
+    // The function to do the changing should also get called here
+    $('#city-type').change(cityBackground);
    
     //when user selects a city in the dropdown, get the value of that city
     function cityBackground(event){
-		event.preventDefault();
-		var cities = $('#city-type').val(); 
+      event.preventDefault();
+      // You should use a different variable name because you want it to do something different
+      var city = $('#city-type').val();
+      console.log(city);
 
-		console.log(cities)
-	}
+      // This if statement needs to be called within the function so it knows when to run.  When it's outside the function, it runs without a trigger
+      if (city === 'NYC') {
+        // Show the NYC background
+        $('body').addClass('nyc');
+      } else if (city === 'SF') {
+        // Show the SF background
+        $('body').addClass('sf');
+      } else if (city === 'LA') {
+        // Show the LA background
+        $('body').addClass('la');
+      } else if (city === 'ATX') {
+        // Show the Austin background
+        $('body').addClass('austin');
+      } else if (city === 'SYD') {
+        // Show the LA background
+        $('body').addClass('sydney');
+      } else {
+        // Remove all classes from body to show the default background.
+        $('body').removeClass();
+      }
+
+    }
 
 	//match the value of that city with the relevant index
 
-	if (cities === 'NYC') {
-		// Show the NYC background
-		$('body').addClass('nyc');
-	} else if (cities === 'SF') {
-		// Show the SF background
-		$('body').addClass('sf');
-	} else if (cities === 'LA') {
-		// Show the LA background
-		$('body').addClass('la');
-	} else if (cities === 'ATX') {
-		// Show the LA background
-		$('body').addClass('austin');
-	} else if (cities === 'SYD') {
-		// Show the LA background
-		$('body').addClass('sydney');
-	} else {
-		// Remove all classes from body to show the default background.
-		$('body').removeClass();
-	}
-
+	
 	
 });
 
